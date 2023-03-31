70f948637ccab23abe4c564155d20e66c69d0fc4
daniellarikov <daniklarikov@gmail.com>
2023-03-31 22:45:03 +0300

    first commit

---
+++ kurskayabitva/main/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+urlpatterns = [
+    path('', views.index, name='home'),
+    path('inform_page', views.tech, name='ground'),
+    path('airplanes', views.airplanes, name='airplanes'),
+    path('fire_guns', views.fire_guns, name='fire_guns')
+]
---
+++ kurskayabitva/main/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+def index(request):
+    return render (request, 'main/main-page-total.html')
+
+def tech(request):
+    return render (request, 'main/ground.html')
+
+def airplanes(request):
+    return render (request, 'main/airplanes.html')
+
+def fire_guns(request):
+    return render (request, 'main/fire_guns.html')
